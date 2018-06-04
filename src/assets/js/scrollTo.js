03f3408b4903ec4b9aead2800262c6c1f23c7c18
Ricardo Gimenes Ferreira <gimenesferreira@gmail.com>
2018-06-04 08:52:32 -0300

    Creating the scroll to animation

---
+++ src/assets/js/scrollTo.js
@@ -0,0 +1,22 @@
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+        
+    var animateScroll = function(){        
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad=function(n,t,u,a){return(n/=a/2)<1?u/2*n*n+t:-u/2*(--n*(n-2)-1)+t};
