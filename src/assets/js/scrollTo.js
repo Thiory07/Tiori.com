0ab37eacd399a496d3ae8fa6dd3f6e5d9721cdc4
Ricardo Gimenes Ferreira <gimenesferreira@gmail.com>
2018-06-04 09:10:01 -0300

    using adjosh scroll to function
    gist URL: https://gist.github.com/andjosh/6764939

--- src/assets/js/scrollTo.js
+++ src/assets/js/scrollTo.js
@@ -1,22 +1,29 @@
-function scrollTo(element, to, duration) {
-    var start = element.scrollTop,
-        change = to - start,
-        currentTime = 0,
-        increment = 20;
-        
-    var animateScroll = function(){        
-        currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
+scrollTo = function(to, duration) {
+    const
+    element = document.scrollingElement || document.documentElement,
+    start = element.scrollTop,
+    change = to - start,
+    startDate = +new Date(),
+    // t = current time
+    // b = start value
+    // c = change in value
+    // d = duration
+    easeInOutQuad = function(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    },
+    animateScroll = function() {
+        const currentDate = +new Date();
+        const currentTime = currentDate - startDate;
+        element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
         if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
+            requestAnimationFrame(animateScroll);
+        }
+        else {
+            element.scrollTop = to;
         }
     };
     animateScroll();
-}
-
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad=function(n,t,u,a){return(n/=a/2)<1?u/2*n*n+t:-u/2*(--n*(n-2)-1)+t};
+};
