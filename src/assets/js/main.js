55434872405358c6c0906aaab0c840de7fb20c78
Thiory007 <gimenesferreira@gmail.com>
2018-06-05 23:48:02 -0300

    rewritting the js without jQuery

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -95,7 +95,26 @@ $(function () {
 });
 
 
-var languages = document.getElementById("languages");
-languages.addEventListener("click", function(e) {
-	languages.classList.toggle("show");
+document.addEventListener("DOMContentLoaded", function(e) {
+	//languages button
+	var languages = document.getElementById("languages");
+	languages.addEventListener("click", function(e) {
+	  this.classList.toggle("show");
+	});
+
+	//onscroll event
+	window.addEventListener('scroll', throttle(highlightNavigation, 500));
+	function throttle(fn, wait) {
+	  var time = Date.now();
+	  return function() {
+		if ((time + wait - Date.now()) < 0) {
+		  fn();
+		  time = Date.now();
+	    }
+	  }
+	}
+	  
+	function highlightNavigation() {
+	  console.log("scroll fired");
+	}
 });
